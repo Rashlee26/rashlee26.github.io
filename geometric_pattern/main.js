63c9c70890acadfe10f993d9f9939ecd36254b18
Ryan <26rashlee@gmail.com>
2017-02-01 20:06:47 +0000

    Added jarring animation to geometric_pattern

--- geometric_pattern/main.js
+++ geometric_pattern/main.js
@@ -1,11 +1,23 @@
 var NUM_CIRCLES = 12;
+
 var circleDiameter;
 var circleRadius;
 
+var rVal;
+var gVal;
+var bVal;
+
 function setup() {
     createCanvas(480, 600);
+    
+    frameRate(5);
+    
     circleDiameter = width/NUM_CIRCLES;
     circleRadius = circleDiameter/2;
+    
+    rVal = 255;
+    gVal = 0;
+    bVal = 0;
 }
 
 function draw() {
@@ -23,14 +35,24 @@ function draw() {
     }
     
     while (x <= width) {
-        fill(color(255, 0, 0));
+        stroke(color(rVal, gVal, bVal));
+        fill(color(rVal, gVal, bVal));
         ellipse(x, y, circleDiameter, circleDiameter);
         x = x + circleDiameter;
     }
     
-    y = y + circleRadius;
+    y = y - circleRadius;
     isShifted = !isShifted;
-    
-    }    
+
+     rVal = (rVal + 254) % 256;
+    gVal = (gVal + 7) % 256;
+    bVal = (bVal + 3) % 256;
     }
-    
+}
+
+
+function keyPressed() {
+    if (keycode === 115 || keyCode === 83) {
+        saveCanvas('geometricPattern', 'png');
+    }
+}
